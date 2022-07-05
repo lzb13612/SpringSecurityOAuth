3e5209da1ff74deb3c0ed0e007aa10fa4b2d7c82
liuzhibin <1072095696@qq.com>
2022-07-05 17:05:44 +0800

    init

---
+++ src/test/java/com/sso/springsecurityoauth/SpringSecurityOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sso.springsecurityoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
